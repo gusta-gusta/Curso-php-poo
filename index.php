c5fcaf694b492d95fe415b9538fd6af2a762d7a6
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2022-08-17 21:29:32 -0300

    Olá mundo
    inicio de um senho

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste PHP</title>
+</head>
+<body>
+    <?php
+    #Gustaaa
+        echo 'Olá mundo!';
+        phpinfo();
+    ?>
+
+</body>
+</html>

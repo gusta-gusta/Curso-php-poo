597253778da5cfcccd260c00cbff246361665f56
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2022-08-20 21:46:58 -0300

    Aula 01
    Obj Caneta

--- index.php
+++ index.php
@@ -7,9 +7,22 @@
 </head>
 <body>
     <?php
-    #Gustaaa
-        echo 'Olá mundo!';
-        phpinfo();
+        require_once 'caneta.php'; //incluir um arquivo PHP em outro
+
+        $c1 = new Caneta;
+        $c1->cor = "Azul";
+        $c1->ponta = 0.5;
+        $c1-> tampada = false;
+        $c1 -> tampar();
+
+        $c1->rabiscar();
+
+        $c2 =  new Caneta;
+        $c2->cor = "Preto";
+        $c2->ponta = 1;
+        $c2->tampada = false;
+
+        $c2->rabiscar();
     ?>
 
 </body>

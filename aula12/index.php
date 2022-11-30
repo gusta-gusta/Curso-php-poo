98499cd89addbb4796ac75062f1956f3fde9561d
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2022-11-30 12:55:16 -0300

    Att aula12
    1.1

--- aula12/index.php
+++ aula12/index.php
@@ -11,9 +11,22 @@
         <?php
             require_once 'animal.php';
             require_once 'mamifero.php';
-            $M = new mamifero();
+            require_once 'reptil.php';
+            require_once 'peixe.php';
+            require_once 'ave.php';
+            # Mamifero
+            $M = new Mamifero();
             $M->Locomover();
             print_r($M);
+            # Reptil
+            $R = new Reptil();
+            print_r($R);
+            # Peixe
+            $P = new Peixe();
+            print_r($P);
+            #Ave
+            $A = new Ave();
+            print_r($A);
         ?>
     </pre>
 </body>
